8696242462740244be5c7dbe461a4c9b45ee44b6
sagvekartanvi7 <sagvekartanvi7@gmail.com>
2026-08-12 12:44:22 +0530

    Implemented prediction workflow

--- Frontend/script.js
+++ Frontend/script.js
@@ -28,3 +28,50 @@ predictBtn.addEventListener("click", async () => {
     }
 
 });
+
+
+const customBtn = document.getElementById("customPredictBtn");
+const customPrediction = document.getElementById("customPrediction");
+
+customBtn.addEventListener("click", async () => {
+
+    const payload = {
+        country: document.getElementById("country").value,
+        shipmentMode: document.getElementById("shipmentMode").value,
+        weight: document.getElementById("weight").value,
+        freightCost: document.getElementById("freightCost").value,
+    };
+
+    try {
+
+        const response = await fetch("http://127.0.0.1:8000/predict_custom", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(payload),
+        });
+
+        const data = await response.json();
+
+        if (data.error) {
+            customPrediction.innerHTML = "❌ " + data.error;
+            customPrediction.style.color = "orange";
+        }
+        else if (data.prediction === 1) {
+            customPrediction.innerHTML = "⚠️ Shipment Delay Expected";
+            customPrediction.style.color = "red";
+        }
+        else {
+            customPrediction.innerHTML = "✅ Shipment On Time";
+            customPrediction.style.color = "green";
+        }
+
+    }
+
+    catch (error) {
+
+        customPrediction.innerHTML = "❌ Backend not running";
+        customPrediction.style.color = "orange";
+
+    }
+
+});
